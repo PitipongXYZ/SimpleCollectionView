f78efbe3e74c75099ee9fac5e3eb7a406c466024
Manao <manao@manaos-mac-mini.local>
2016-04-25 16:32:14 +0700

    Initial Commit

---
+++ SimpleCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCollectionView
+//
+//  Created by Manao on 4/25/16.
+//  Copyright © 2016 Pitipong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
